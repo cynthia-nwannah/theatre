7c387c665c3c3ed75b8860421e506ae7d0b89e38
Cynthia nwannah <chinwendunwannah@yahoo.com>
2023-05-20 22:53:59 +0100

    practice my javascript
    practice my javascript

---
+++ index.js
@@ -0,0 +1,18 @@
+let incri = document.getElementById("start");
+let savEl = document.getElementById("entries");
+
+let count = 0;
+
+ function increase(){
+    count += 1;
+    incri.textContent = count;
+ }
+
+ function save() {
+
+    let countstr = " + " + count
+    savEl.textContent += countstr
+    incri.textContent = 0;
+    count = 0;
+ }
+
